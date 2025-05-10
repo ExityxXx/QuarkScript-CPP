80d51f9385737f1dbe6340f210ed6e35217b0b42
exityx <gdlasorum@gmail.com>
2025-05-10 12:10:20 +0300

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 
 enum class TokenType {
     Plus=0, Minus, Multiply, Divide=3,
+    Int=4, Float
 };
 
 struct Token {
@@ -14,24 +16,23 @@ struct Token {
 std::string token_type_to_string(TokenType &token_type) {
     switch (token_type)
     {
-    case TokenType::Plus:
-        return "Plus";
-    case TokenType::Minus:
-        return "Minus";
-    case TokenType::Multiply:
-        return "Multiply";
-    case TokenType::Divide:
-        return "Divide";
+    case TokenType::Plus: return "Plus";
+    case TokenType::Minus: return "Minus";
+    case TokenType::Multiply: return "Multiply";
+    case TokenType::Divide: return "Divide";
+    case TokenType::Int: return "Int";
+    case TokenType::Float: return "Float";
     default:
         return "Unknown";
     }
 }
 class Lexer {
     private:
+        // Variables
         std::string text;
         int position = 0;
         char current_char;
-        // Functions
+        // Advance Function
         void advance() {
             ++position;
             if (text.length() > position) current_char = text[position];
@@ -45,10 +46,26 @@ class Lexer {
         }
 
         
-    
+        // Number Generate Function
+        Token numberGenerate() {
+            std::string result;
+            bool dot_count = false;
+
+            while (current_char && (current_char >= '0' && current_char <= '9') || current_char == '.') {
+                if (current_char == '.') {
+                    if (dot_count) break;
+                    dot_count = true;
+
+                }
+                result += current_char;
+                advance();
+            }
+
+            return dot_count ? Token{TokenType::Float, result} : Token{TokenType::Int, result};
+        }
         // Tokenize Function
         std::vector<Token> tokenize() {
-            std::vector <Token> tokens;
+            std::vector<Token> tokens;
             while (current_char) {
                 if (current_char == ' ' || current_char == '\t') {
                     advance();
@@ -75,16 +92,31 @@ class Lexer {
                     advance();
                     continue;
                 }
+                if (current_char >= '0' || current_char <= '9') {
+                    Token generated_number = numberGenerate();
+                    tokens.push_back(generated_number);
+                    continue; 
+                }
             }
             return tokens;
         }
 };
-int main() {
-    Lexer tokens("++ - / *");
-    std::vector<Token> tokenize_result = tokens.tokenize();
 
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    Lexer tokens("5 + 5 - 10 * 2 / 50.5 * 234 3029894098320948320948320948 * 342 ");
+    std::vector<Token> tokenize_result = tokens.tokenize();
+    
     for (Token &element : tokenize_result) {
-        std::cout << "Token(type=" << token_type_to_string(element.type) << ", value='" << element.value << "')\n"; 
+        std::cout << "Token(type=" + token_type_to_string(element.type) + ", value'" + element.value + "')\n";
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
+
+    return 0;
 }
 
